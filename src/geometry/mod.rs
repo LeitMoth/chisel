239bc9db4ca0b207d47011fab942eeafe64aabe4
LeitMoth <col.phillips2004@gmail.com>
2024-06-08 21:28:13 -0500

    update intersection_point math

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -25,44 +25,33 @@ impl StandardPlane {
 
         let w = p.dot(c);
 
+        if w.abs() > 20_000_000.0 {
+            dbg!(&(p,q,r,c,w));
+        }
+
         StandardPlane { normal: c, d: w }
     }
 
     pub fn intersection_point(&self, p1: &Self, p2: &Self) -> Option<Vec3> {
-        // https://en.wikipedia.org/wiki/Cramer%27s_rule
-
-        // Keep in mind these arrays are columns. This would have been much cleaner if I could make one out of rows...
-        let s = Mat3::from_cols_array_2d(&[
-            [self.normal.x, p1.normal.x, p2.normal.x],
-            [self.normal.y, p1.normal.y, p2.normal.y],
-            [self.normal.z, p1.normal.z, p2.normal.z],
-        ]);
+        let s = Mat3 {
+            x_axis: self.normal,
+            y_axis: p1.normal,
+            z_axis: p2.normal,
+        }.transpose();
 
         let d_col = Vec3::new(self.d, p1.d, p2.d);
 
-        let mut x_top = s;
-        *x_top.col_mut(0) = d_col;
-
-        let mut y_top = s;
-        *y_top.col_mut(1) = d_col;
-
-        let mut z_top = s;
-        *z_top.col_mut(2) = d_col;
-
-        let s_det = s.determinant();
-
-        if s_det == 0.0 {
-            // println!("{self:?} <> {p1:?} <> {p2:?}");
-
-            // println!("Det was 0!!!");
-            return None;
+        if s.determinant() == 0.0 {
+            None
+        } else {
+            let x = s.inverse() * d_col;
+            // if x.length() > 20_000.0 {
+            //     None
+            // } else {
+            //     Some(x)
+            // }
+            Some(x)
         }
-
-        let x = x_top.determinant() / s_det;
-        let y = y_top.determinant() / s_det;
-        let z = z_top.determinant() / s_det;
-
-        Some(Vec3::new(x, y, z))
     }
 }
 
